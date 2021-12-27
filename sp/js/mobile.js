876f6ca9e97311c1625a0fab86f373842cee9dcd
Jitsu <60904693+anosu@users.noreply.github.com>
2021-12-27 12:56:14 +0800

    Update mobile.js

--- sp/js/mobile.js
+++ sp/js/mobile.js
@@ -2,6 +2,7 @@ var art = new Artplayer({
     container: '.artplayer-app',
     url: 'https://alist.jitsu.top/d/OneDrive/视频/里番 リアルエロゲシチュエーション！/[ピンクパイナップル]リアルエロゲシチュエーション！2 THE ANIMATION 第1巻/[GB][ピンクパイナップル]リアルエロゲシチュエーション！2 THE ANIMATION 第1巻[AVC-1080P].mp4',
     autoSize: true,
+    autoplay: true,
     setting: true,
     screenshot: true,
     playbackRate: true,
