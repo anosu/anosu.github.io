7f638a16acf49d8f4f626c4cb45ba854b5f7bae5
Jitsu <60904693+anosu@users.noreply.github.com>
2022-02-04 16:05:45 +0800

    Delete cdn/alist directory

--- cdn/alist/v2/2/assets/useTitle.dd9a790e.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./vendor.38c2f4fc.js";var e=(e,o=!1)=>{t.exports.useEffect((()=>{const t=document.title;return document.title=o?`${e} - alist`:e,()=>{document.title=t}}),[])};export{e as u};
