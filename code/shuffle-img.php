a079e32aedd9fa20e26d75d1e68e4a539b59d72c
Jitsu <60904693+anosu@users.noreply.github.com>
2022-06-05 01:12:33 +0800

    Update shuffle-img.php

--- code/shuffle-img.php
+++ code/shuffle-img.php
@@ -12,6 +12,24 @@ $data = file_get_contents($filename);
 $data = explode(PHP_EOL, $data);
 //获取类型和数量
 $type = $_GET['type'];
+if($sort == 'r18'){
+// 定义r18内容
+$type = 'json';
+$text = 'To prevent abuse, r18 always returns json';
+$proxy = !empty($_GET['proxy']) ? $_GET['proxy'] : 'i.loli.best' ;
+$size_arr = array('original', 'regular', 'small', 'thumb', 'mini');
+$size = !empty($_GET['size']) ? $_GET['size'] : 'regular' ;
+if(!in_array($size, $size_arr)){
+	$size = 'original';
+}
+} else {
+// 定义常规内容
+$size_arr = array('large', 'mw2048', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb150', 'thumb180', 'thumbnail', 'orj360', 'orj480', 'square');
+$size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
+if(!in_array($size, $size_arr)){
+	$size = 'large';
+}
+}
 if($type == 'json'){
 $num = !empty($_GET['num']) ? $_GET['num'] : '1' ;
 } else {
@@ -26,34 +44,24 @@ $result = $data[array_rand($data)];
 // 去除多余的换行符（解决获取空值问题
 $result = str_replace(array("\r","\n","\r\n"), '', $result);
 if($sort == 'r18'){
-// 定义r18内容
-$proxy = !empty($_GET['proxy']) ? $_GET['proxy'] : 'i.loli.best' ;
-$size_arr = array('original', 'regular', 'small', 'thumb', 'mini');
-$size = !empty($_GET['size']) ? $_GET['size'] : 'regular' ;
-if(!in_array($size, $size_arr)){
-	$size = 'original';
-}
+
 $value = explode('_',$result);
 $url = 'https://'.$proxy.'/'.$size.'/'.$value[0].'/'.$value[1].'';
 } else {
-// 定义常规内容
-$size_arr = array('large', 'mw2048', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb150', 'thumb180', 'thumbnail', 'orj360', 'orj480', 'square');
-$size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
+
 $server = rand(1,4);
-if(!in_array($size, $size_arr)){
-	$size = 'large';
-}
+
 $url = 'https://tva'.$server.'.sinaimg.cn/'.$size.'/'.$result.'.jpg';
 }
 array_push($urls, $url);
 $n++;
 }
-while ($n<$num);
+while ($n<=$num);
 //返回指定格式
 switch($type){
 case 'json':
 header('Content-type:text/json');
-die(json_encode(['sort'=>$sort,'number'=>$num,'pic'=>$urls]));
+die(json_encode(['sort'=>$sort,'number'=>$num,'pic'=>$urls,'warning'=>$text],JSON_PRETTY_PRINT));
 default:
 die(header("Location: $url"));
 }
